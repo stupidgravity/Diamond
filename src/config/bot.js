edf5a80f7d2f5d41d47cfca99b79c1c404b59966
Gravity <wooddominic95@gmail.com>
2026-07-29 01:12:17 -0500

    Change bot status message to '/help'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "The Official Glaze Bot",     // this is what people actually see
+        state: "/help",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
