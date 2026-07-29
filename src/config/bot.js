2d7cf42a42be61fd203443685cc1dffa90a08ac5
Gravity <wooddominic95@gmail.com>
2026-07-29 01:24:35 -0500

    Update bot activity state and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "/Help",     // this is what people actually see
-        type: 2,               // Custom
+        state: "The Official Diamond Bot",     // this is what people actually see
+        type: 4,               // Custom
       },
     ],
   },
