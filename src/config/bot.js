e473a62ec8e225851aa6398ca3aeb64ff7c829a4
Gravity <wooddominic95@gmail.com>
2026-07-28 22:13:59 -0500

    Change bot status message to 'Nothing Happend'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "I LOVE NOAH",     // this is what people actually see
+        state: "Nothing Happend",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
