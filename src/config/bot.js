6774394d8fa220b14bd24b4cf47164bd1826b824
Gravity <wooddominic95@gmail.com>
2026-07-29 14:05:19 -0500

    Update bot status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Iris Is Nice",     // this is what people actually see
+        state: "IYou arent safe",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
