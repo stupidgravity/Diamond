9ed0b6c996b1413e0941775c44a0be6e73557945
Gravity <wooddominic95@gmail.com>
2026-07-28 22:11:35 -0500

    Update bot status from 'I LOVE KYRO' to 'I LOVE NOAH'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "I LOVE KYRO",     // this is what people actually see
+        state: "I LOVE NOAH",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
